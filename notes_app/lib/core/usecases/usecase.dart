0344b16a674afcf0e4e97e3a0005fb89ea2965ba
Beverly Mugwadi <b.mugwadi1@alustudent.com>
2025-07-06 19:30:36 +0200

    Adding the usecase

---
+++ notes_app/lib/core/usecases/usecase.dart
@@ -0,0 +1,28 @@
+// lib/core/usecases/usecase.dart (UPDATED)
+
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:notes_app/core/errors/failures.dart';
+
+// Abstract class for synchronous or Future-based use cases
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
+}
+
+// Abstract class for Stream-based use cases
+abstract class StreamUseCase<Type, Params> {
+  Stream<Type> call(Params params);
+}
+
+// NEW: Abstract class for use cases that take no parameters and return a direct type (not a Future or Stream of Either)
+abstract class NoParamsUseCase<Type> {
+  Type call(NoParams params);
+}
+
+
+class NoParams extends Equatable {
+  const NoParams();
+
+  @override
+  List<Object?> get props => [];
+}
